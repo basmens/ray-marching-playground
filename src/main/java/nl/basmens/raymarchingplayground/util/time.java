c427b52af3d5175834a9114834598cb934c454e0
Ben <Ben.mens@gmail.com>
2022-07-28 21:42:56 +0200

    Delete time.java

--- src/main/java/nl/basmens/raymarchingplayground/util/time.java
+++
@@ -1,9 +0,0 @@
-package nl.basmens.raymarchingplayground.util;
-
-public class Time {
-  public static double timeStarted = System.nanoTime();
-
-  public static double getTime() {
-    return (System.nanoTime() - timeStarted) * 1E-9;
-  }
-}
